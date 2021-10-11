a84b3c49d4c9bce1b53d6303171d79055e6b373b
William <william.frisch.moller@gmail.com>
2021-10-11 21:29:54 +0200

    initial commit with pulpo settings and a ps script to copy to cfg

---
+++ copy_to_cfg.ps1
@@ -0,0 +1,13 @@
+$cfg_path="C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\"
+$files="autoexec.cfg","BotPractice.cfg","overwatch.cfg","William.cfg"
+
+$folders="functions"
+$git_path=Get-Location
+
+foreach ($i in $files){
+   Copy-Item -Path ($git_path.Path + "\" + $i) -Destination ($cfg_path) -PassThru
+}
+
+foreach ($i in $folders){
+   Copy-Item -Path ($git_path.Path + "\" + $i + "\") -Destination ($cfg_path) -Recurse -PassThru
+}
